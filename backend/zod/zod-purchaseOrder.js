6ea8c20fe57dc50110931757329b6ed09a1898db
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-04 21:44:46 +0530

    Implemented input validation for purchase order using zod.

---
+++ backend/zod/zod-purchaseOrder.js
@@ -0,0 +1,17 @@
+const zod = require("zod");
+
+const zodPurchaseOrder = zod.object({
+    poNumber: zod.string(),
+    vendor: zod.string(),
+    orderDate: zod.date(),
+    deliveryDate: zod.date(),
+    expectedDeliveryDate: zod.date(),
+    items: zod.object({}),
+    quantity: zod.number(),
+    status: zod.enum(["pending", "completed", "canceled"]).optional(),
+    qualityRating: zod.number().optional(),
+    issueDate: zod.date().optional(),
+    acknowledgementDate: zod.date().optional()
+})
+
+module.exports = zodPurchaseOrder;
