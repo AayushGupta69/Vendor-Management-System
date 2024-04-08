35c2b0594b08e064f6aec87d6e44914f5503b306
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-08 22:54:09 +0530

    Added SubHeading Component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,11 @@
 import {Heading} from "../components/Heading.jsx";
+import {SubHeading} from "../components/SubHeading.jsx";
 
 export const SignInPage = () => {
     return(
         <div>
             <Heading label="Sign In Page" />
+            <SubHeading label="Enter your credentials to access your account" />
         </div>
     )
 }
