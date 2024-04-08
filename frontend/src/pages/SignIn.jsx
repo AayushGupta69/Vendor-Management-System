b05ce9a6380d43051139ddd1b4f911a7328d204d
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-09 03:30:40 +0530

    Implemented SignIn functionality using Axios and updated InputBox Component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,8 +3,13 @@ import {SubHeading} from "../components/SubHeading.jsx";
 import {InputBox} from "../components/InputBox.jsx";
 import {Button} from "../components/Button.jsx";
 import {BottomWarning} from "../components/BottomWarning.jsx";
+import {useState} from "react";
+import axios from "axios";
 
 export const SignInPage = () => {
+    const [vendorCode, setVendorCode] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
         <div>
             <div className="bg-slate-200 h-screen flex flex-col justify-center items-center">
@@ -18,10 +23,20 @@ export const SignInPage = () => {
                     <div className="rounded-lg bg-white w-80 text-center p-1 h-max px-4">
                         <Heading label="Sign In"/>
                         <SubHeading label="Enter your credentials to sign in"/>
-                        <InputBox label="Vendor Code" placeholder="VND123" onChange={() => {}}/>
-                        <InputBox label="Password" placeholder="vendor@12345" onChange={() => {}}/>
+                        <InputBox label="Vendor Code" placeholder="VND123" type="text" onChange={(e) => {
+                            setVendorCode(e.target.value);
+                        }}/>
+                        <InputBox label="Password" placeholder="vendor@12345" type="password" onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}/>
                         <div className="pt-4">
-                            <Button label="Sign In" onClick={() => {}}/>
+                            <Button label="Sign In" onClick={async () => {
+                                const response = await axios.post("http://localhost:3000/api/vendors/signin", {
+                                    vendorCode,
+                                    password
+                                });
+                                localStorage.setItem("token", response.data.token);
+                            }}/>
                         </div>
                         <BottomWarning label={"Dont have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
                     </div>
