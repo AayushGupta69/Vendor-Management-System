4dc70445f955c7c47dc78b67a92244199a885d1e
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-08 23:05:27 +0530

    Added Button Component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,14 +1,16 @@
 import {Heading} from "../components/Heading.jsx";
 import {SubHeading} from "../components/SubHeading.jsx";
 import {InputBox} from "../components/InputBox.jsx";
+import {Button} from "../components/Button.jsx";
 
 export const SignInPage = () => {
     return(
         <div>
             <Heading label="Sign In" />
             <SubHeading label="Enter your credentials to access your account" />
-            <InputBox label={"Vendor Code"} placeholder={"VND123"} onChange={() => {}} />
-            <InputBox label={"Password"} placeholder={"vendor@12345"} onChange={() => {}} />
+            <InputBox label="Vendor Code" placeholder="VND123" onChange={() => {}} />
+            <InputBox label="Password" placeholder="vendor@12345" onChange={() => {}} />
+            <Button label="Sign In" onClick={() => {}} />
         </div>
     )
 }
