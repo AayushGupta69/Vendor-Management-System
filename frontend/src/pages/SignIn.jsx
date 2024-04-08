81bc69ae3e4894eec3ad0c647547bbbf43daa85e
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-08 23:01:24 +0530

    Added InputBox Component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
 import {Heading} from "../components/Heading.jsx";
 import {SubHeading} from "../components/SubHeading.jsx";
+import {InputBox} from "../components/InputBox.jsx";
 
 export const SignInPage = () => {
     return(
         <div>
-            <Heading label="Sign In Page" />
+            <Heading label="Sign In" />
             <SubHeading label="Enter your credentials to access your account" />
+            <InputBox label={"Vendor Code"} placeholder={"VND123"} onChange={() => {}} />
+            <InputBox label={"Password"} placeholder={"vendor@12345"} onChange={() => {}} />
         </div>
     )
 }
