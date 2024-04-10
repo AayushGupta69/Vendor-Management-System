672502721ed34fb5f6d99a8b4dceb4994a2b8652
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-10 23:16:26 +0530

    Updated SignIn Component UI

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -12,33 +12,33 @@ export const SignInPage = () => {
 
     return (
         <div className="bg-slate-200 h-screen flex flex-col justify-center items-center">
-            <div className="mb-1">
-                <Heading label="Vendor Management System"/>
-            </div>
-            <div>
-                <img src="src/assets/Vendor Management System.webp" alt="Vendor Management System GIF" className="mt-4 mx-auto h-40 w-full mb-10"/>
-            </div>
-            <div className="flex flex-col justify-center">
-                <div className="rounded-lg bg-white w-80 text-center p-1 h-max px-4">
-                    <Heading label="Sign In"/>
-                    <SubHeading label="Enter your credentials to sign in"/>
-                    <InputBox label="Vendor Code" placeholder="VND123" type="text" onChange={(e) => {
-                        setVendorCode(e.target.value);
-                    }}/>
-                    <InputBox label="Password" placeholder="vendor@12345" type="password" onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}/>
-                    <div className="pt-4">
-                        <Button label="Sign In" onClick={async () => {
-                            const response = await axios.post("http://localhost:3000/api/vendors/signin", {
-                                vendorCode,
-                                password
-                            });
-                            localStorage.setItem("token", response.data.token);
-                            localStorage.setItem("role", response.data.role);
+            <div className="flex justify-between">
+                <div className="flex flex-col justify-center">
+                    <Heading label="Vendor Management System"/>
+                    <img src="src/assets/Vendor Management System.webp" alt="Vendor Management System GIF" className="mt-20 mx-auto h-40 w-auto mb-10"/>
+                </div>
+                <div className="pl-28">
+                    <div className="rounded-lg bg-white w-80 text-center p-1 h-max px-4">
+                        <Heading label="Sign In"/>
+                        <SubHeading label="Enter your credentials to sign in"/>
+                        <InputBox label="Vendor Code" placeholder="VND123" type="text" onChange={(e) => {
+                            setVendorCode(e.target.value);
                         }}/>
+                        <InputBox label="Password" placeholder="vendor@12345" type="password" onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}/>
+                        <div className="pt-4">
+                            <Button label="Sign In" onClick={async () => {
+                                const response = await axios.post("http://localhost:3000/api/vendors/signin", {
+                                    vendorCode,
+                                    password
+                                });
+                                localStorage.setItem("token", response.data.token);
+                                localStorage.setItem("role", response.data.role);
+                            }}/>
+                        </div>
+                        <BottomWarning label={"Dont have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
                     </div>
-                    <BottomWarning label={"Dont have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
                 </div>
             </div>
         </div>
