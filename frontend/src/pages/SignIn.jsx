030772af76702fb41359b2076bd01042d0361e5d
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-11 10:33:04 +0530

    Updated SignIn.jsx to implement navigation functionality to dashboard when sign in is successful

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -5,10 +5,12 @@ import {Button} from "../components/Button.jsx";
 import {BottomWarning} from "../components/BottomWarning.jsx";
 import {useState} from "react";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export const SignInPage = () => {
     const [vendorCode, setVendorCode] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     return (
         <div className="bg-blue-600 h-screen flex flex-col justify-center items-center">
@@ -35,6 +37,9 @@ export const SignInPage = () => {
                                 });
                                 localStorage.setItem("token", response.data.token);
                                 localStorage.setItem("role", response.data.role);
+                                if(response.data.role == "User") {
+                                    navigate("/user_dashboard");
+                                }
                             }}/>
                         </div>
                         <BottomWarning label={"Dont have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
