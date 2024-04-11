e3bfab2642fc6f9f1885072c7a50816e3320fc6a
Aayush Gupta <aayushgupta69official@gmail.com>
2024-04-11 19:29:03 +0530

    Updated SignIn Page UI

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -25,28 +25,41 @@ export const SignInPage = () => {
     }
 
     return (
-        <div className="bg-blue-600 h-screen flex flex-col justify-center items-center">
-            <div className="flex justify-between">
-                <div className="flex flex-col justify-center">
-                    <Heading label="Vendor Management System" color="text-white" />
-                    <img src="src/assets/Vendor Management System.webp" alt="Vendor Management System GIF" className="mt-20 mx-auto h-40 w-auto mb-10"/>
-                </div>
-                <div className="pl-28">
-                    <div className="rounded-lg bg-white w-80 text-center p-1 h-max px-4">
-                        <Heading label="Sign In" color="text-blue-500"/>
-                        <SubHeading label="Enter your credentials to sign in"/>
-                        <InputBox label="Vendor Code" placeholder="VND123" type="text" onChange={(e) => {
-                            setVendorCode(e.target.value);
-                        }}/>
-                        <InputBox label="Password" placeholder="vendor@12345" type="password" onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}/>
-                        <div className="pt-4">
-                            <Button label="Sign In" onClick={handleSignIn}/>
+        <div className="flex flex-col min-h-screen">
+            <div className="bg-blue-950 text-white py-2 px-4 text-center text-xl">
+                <h2>Vendor Management System</h2>
+            </div>
+
+            <div className="flex-1">
+                <div className="bg-blue-500 h-screen flex flex-col justify-center items-center">
+                    <div className="flex justify-between">
+                        <div className="flex flex-col justify-center pr-28">
+                            <Heading label="Welcome! Sign in to continue." color="text-white"/>
+                            <img src="src/assets/Vendor Management System.webp" alt="Vendor Management System GIF"
+                                 className="mt-20 mx-auto h-40 w-auto mb-10"/>
                         </div>
-                        <BottomWarning label={"Dont have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
+                        <div className="pl-28">
+                            <div className="rounded-lg bg-white w-80 text-center p-1 h-max px-4">
+                                <Heading label="Sign In" color="text-blue-500"/>
+                                <SubHeading label="Enter your credentials to sign in"/>
+                                <InputBox label="Vendor Code" placeholder="VND123" type="text" onChange={(e) => {
+                                    setVendorCode(e.target.value);
+                                }}/>
+                                <InputBox label="Password" placeholder="vendor@12345" type="password" onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}/>
+                                <div className="pt-4">
+                                    <Button label="Sign In" onClick={handleSignIn}/>
+                                </div>
+                                <BottomWarning label={"Dont have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="bg-blue-950 text-white py-2 px-4 text-center text-xl">
+                <h2>© 2024 Vendor Management System. All rights reserved.</h2>
             </div>
         </div>
     )
