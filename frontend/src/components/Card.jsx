d93c1fb120ea92fe311d59ed1f132cfcf438ac7c
Aayush Gupta <aayushgupta69official@gmail.com>
2024-05-28 21:07:26 +0530

    Added a new Card component and updated user dashboard.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,9 +1,19 @@
 import {Link} from "react-router-dom"
 
-export const Card = ({label, to}) => {
+export const Card1 = ({label, to}) => {
     return (
         <Link to={to}>
             <div className="rounded-lg bg-slate-100 h-44 w-44 p-8 shadow-lg drop-shadow-lg text-center hover:shadow-xl transition duration-300 transform hover:scale-110">
+                <div className="font-bold text-xl text-blue-600">{label}</div>
+            </div>
+        </Link>
+    )
+}
+
+export const Card2 = ({label, to}) => {
+    return (
+        <Link to={to}>
+            <div className="rounded-lg bg-slate-100 h-60 w-full p-8 shadow-lg drop-shadow-lg text-center hover:shadow-xl transition duration-300 transform hover:scale-110">
                 <div className="font-bold text-xl text-blue-600">{label}</div>
             </div>
         </Link>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import {Sidebar} from "../components/Sidebar.jsx";
 import {Heading} from "../components/Heading.jsx";
-import {Card} from "../components/Card.jsx";
+import {Card1, Card2} from "../components/Card.jsx";
 import {BottomNavBar} from "../components/BottomNavBar.jsx";
 import {TopNavBarWithLogout} from "../components/TopNavBarWithLogout.jsx";
 import {useEffect, useState} from "react";
@@ -24,16 +24,22 @@ export const UserDashboard = () => {
                 <div className="text-center">
                     <Heading label={`Welcome${name ? `, ${name}` : ''}!`} color="text-blue-600"/>
                 </div>
-                <div className="flex ml-44 mt-10 mr-32 justify-around">
-                    <Card label="Create New Purchase Order" to="/create_purchase_order"/>
-                    <Card label="Update Existing Purchase Order" to="/update_purchase_order"/>
-                    <Card label="View All Purchase Orders" to="/view_all_purchase_order"/>
-                    <Card label="View Specific Purchase Order" to="/view_purchase_order"/>
-                    <Card label="Delete Purchase Order" to="/delete_purchase_order"/>
+
+                <div className="flex flex-col flex-1">
+                    <div className="flex ml-44 mt-10 mr-32 justify-around">
+                        <Card1 label="Create New Purchase Order" to="/create_purchase_order"/>
+                        <Card1 label="Update Existing Purchase Order" to="/update_purchase_order"/>
+                        <Card1 label="View All Purchase Orders" to="/view_all_purchase_order"/>
+                        <Card1 label="View Specific Purchase Order" to="/view_purchase_order"/>
+                        <Card1 label="Delete Purchase Order" to="/delete_purchase_order"/>
+                    </div>
+                    <div className="mt-8 ml-48 mr-36 flex-grow">
+                        <Card2 label="Vendor Performance Metrics" to="/vendor_performance_metrics"/>
+                    </div>
                 </div>
             </div>
 
-            <BottomNavBar />
+            <BottomNavBar/>
         </div>
     )
 }
